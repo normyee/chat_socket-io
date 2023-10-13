1b7a7dbc1b444dc985ada6cd1ac08eb3e3d1b75a
normyee <lunoqro@gmail.com>
2023-10-13 20:41:53 -0300

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,44 @@
 import "./App.css";
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io("http://localhost:8000");
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    if (message.trim() !== "") {
+      socket.emit("send_message", { message: message });
+      setMessages([...messages, { text: message, type: "sent" }]);
+    }
+  };
+
+  useEffect(() => {
+    socket.on("received_message", (data) => {
+      setMessageReceived(data.message);
+      setMessages([...messages, { text: data.message, type: "received" }]);
+    });
+  }, [socket, messages]);
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Type your message..."
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Messages: </h1>
+      <div>
+        {messages.map((msg, index) => (
+          <p key={index} className={msg.type}>
+            {msg.text}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
